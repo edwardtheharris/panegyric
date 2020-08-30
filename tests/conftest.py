ce45c14ba944a073838199ce78875de4ec3376e2
Edward Harris <plebis@hotmail.com>
2020-08-30 14:49:35 -0700

    Add fixture for class
    See #6

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+
+import textmywife
+
+def text_mw():
+    """Return an instance of TextMyWife."""
+    return textmywife.TextMyWife()
--- tests/test_text.py
+++ tests/test_text.py
@@ -6,35 +6,30 @@ import textmywife
 class TestTextMyWife:
     """Test class for text my wife package."""
 
-    @classmethod
-    def setup_method(cls):
-        """Setup class for test execution."""
-        text_mw = textmywife.TextMyWife()
-
-    def test_get_message(self):
+    def test_get_message(self, text_mw):
         """Test message retrival."""
         pass
 
-    def test_add_send_date(self):
+    def test_add_send_date(self, text_mw):
         """Test recording of most recent send date."""
         pass
 
-    def test_api_auth(self):
+    def test_api_auth(self, text_mw):
         """Verify authentication with the API."""
         pass
 
-    def test_get_request(self):
+    def test_get_request(self, text_mw):
         """Test that the request is correctly assembled."""
         pass
 
-    def test_api_response(self):
+    def test_api_response(self, text_mw):
         """Validate that the API response is what we expect."""
         pass
 
-    def test_message_iteration(self):
+    def test_message_iteration(self, text_mw):
         """Test that the same message isn't sent twice."""
         pass
 
-    def test_message_rate(self):
+    def test_message_rate(self, text_mw):
         """Verify that we send one message a day."""
         pass

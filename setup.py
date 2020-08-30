95e96f91cc910df77a9372110c2c1da8c9c3fdc1
Edward Harris <plebis@hotmail.com>
2020-08-30 14:04:27 -0700

    Add license
    See #6

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name='Text My Wife',
+    description='Automation for complimenting my wife.',
+    author='Edward Harris',
+    author_email='plebis@hotmail.com',
+    version='v0.1.0',
+)

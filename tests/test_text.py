a0de6df52bc75d25b5808b4c33b8e92cbdf2c97d
Edward Harris <plebis@hotmail.com>
2020-08-30 14:37:58 -0700

    Add git ignore
    See #6

--- tests/test_text.py
+++ tests/test_text.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import textmywife
+
 class TestTextMyWife:
     """Test class for text my wife package."""
 
@@ -9,4 +11,8 @@ class TestTextMyWife:
 
     def test_add_send_date(self):
         """Test recording of most recent send date."""
+        pass
+
+    def test_api_auth(self);
+        """Verify authentication with the API."""
         pass

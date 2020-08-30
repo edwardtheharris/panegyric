bab575147501a75cad409cc7fdc9af64f37dfe45
Edward Harris <plebis@hotmail.com>
2020-08-30 12:52:03 -0700

    Add stub tests
    See #13

---
+++ tests/test_text.py
@@ -0,0 +1,12 @@
+class TestTextMyWife:
+    """Test class for text my wife package."""
+
+    def test_get_message():
+        """Test message retrival."""
+        pass
+
+    def test_add_send_date():
+        """Test recording of most recent send date."""
+        pass
+
+    

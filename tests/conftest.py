9e14d036fe033973f20cbb01989ceb7df03b3a26
Edward Harris <plebis@hotmail.com>
2020-09-06 12:02:11 -0700

    Add fixtures
    See #6

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
+import pytest
 import textmywife
 
 
+@pytest.fixture
 def text_mw():
     """Return an instance of TextMyWife."""
     return textmywife.TextMyWife()
 
 
+@pytest.fixture
 def message():
     """Return message for testing."""
     return {'from': 'harry s truman', 'text': 'the buck stops here'}

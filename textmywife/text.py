d611bc35922d3495c3900545825418c44142e17e
Xander Harris <plebis@hotmail.com>
2021-07-26 18:40:58 -0700

    Add stub methods
    See #7

---
+++ textmywife/text.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+print("textmywife")
+
+
+class TextMyWife:
+    def __init__(self):
+        """Initialize a TextMyWife instance."""
+        pass
+
+    def get_message(self):
+        """Get a message to send."""
+        return
+
+    def get_all_messages(self):
+        """Get all messages."""
+        return
+    
+    def add_send_date(self):
+        """Append most recent send date."""
+        return
+    
+    def send_message(self):
+        """Send the selected message."""
+        return
+
+    def iterate_message(self):
+        """Iterate message list."""
+        return

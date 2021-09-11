8537c7f9d49b321b6ca6ec470ec73c60b5827c9d
Xander Harris <xandertheharris@gmail.com>
2021-09-11 10:35:08 -0700

    Add sentry sdk import and config
    Update tests
    
    See #24

--- src/panegyric/__init__.py
+++ src/panegyric/__init__.py
@@ -1,13 +1,2 @@
 #!/usr/bin/env python3
 """The main panegyric package."""
-
-import sentry_sdk
-sentry_sdk.init(
-    ("https://a40e278a662e46db86ef8aa4d7a46fbd@o325200"
-     ".ingest.sentry.io/5955114"),
-
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0
-)
--- src/panegyric/text.py
+++ src/panegyric/text.py
@@ -5,8 +5,19 @@ import pprint
 import sys
 
 import requests
+import sentry_sdk
 
 from ruamel import yaml
+
+sentry_sdk.init(
+    ("https://a40e278a662e46db86ef8aa4d7a46fbd@o325200"
+     ".ingest.sentry.io/5955114"),
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0
+)
 
 
 class Text:

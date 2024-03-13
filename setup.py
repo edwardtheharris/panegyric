3066997d8dd1cc4dff05f9f4bda2fc41ba05bdf0
Xander Harris <xandertheharris@gmail.com>
2024-03-12 20:02:54 -0700

    create and publish the docuemntation pages
    See #31
    
    @edwardtheharris - xandertheharris@gmail.com
    
    Xander Harris
    
    Changelog: changed

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 """Setup tools configuration."""
 
+import pathlib
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
+def get_version():
+    """Get the project's version."""
+    vpath = pathlib.Path('version')
+    with vpath.open('r', encoding='utf-8') as v_fh:
+        version = v_fh.read()
+    return version
+
+with pathlib.Path('readme.md').open("r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
@@ -26,5 +34,5 @@ setuptools.setup(
         "Bug Tracker": "https://github.com/edwardtheharris/panegyric/issues",
     },
     url="https://github.com/edwardtheharris/panegyric",
-    version='v0.1.0',
+    version=get_version(),
 )
